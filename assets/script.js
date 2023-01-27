66cff9482d5a0b2aadde28976032d16efb02a573
David Keim <dmk252@cornell.edu>
2023-01-27 15:13:26 -0500

    added interface and start message functionality

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,31 @@ var borderTop = 0;
 var borderBottom = 400;
 var ballWidth = 20;
 var ballHeight = 20;
+var gameRunning = false;
+var refreshIntervalID = 0;
 
+document.getElementById("status").innerHTML = "Press space to start";
 root.style.setProperty('--positionX', currentX.toString() + 'px');
 root.style.setProperty('--positionY', currentY.toString() + 'px');
-var refreshIntervalID = setInterval(gameUpdate, 30)
+
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'Space') {
+        if (gameRunning === false) {
+            document.getElementById("status").innerHTML = "";
+            gameRunning = true;
+            runGame();
+        }
+        else {
+            clearInterval(refreshIntervalID);
+            gameRunning = false;
+            document.getElementById("status").innerHTML = "Paused";
+        }
+    }
+  });
+
+function runGame() {
+    refreshIntervalID = setInterval(gameUpdate, 30)
+}
 
 function gameUpdate() {
     currentX = currentX + velocityX;
